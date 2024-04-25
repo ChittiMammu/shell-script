53e49e8aade4d1b62a390dde4f791fe1e7cb0819
ChittiMammu <mammu.malluru@gmail.com>
2024-04-25 19:47:52 +0530

    my first attempt

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=linga
+PERSON2=mamatha
+
+echo "$PERSON1 ,hi will you teach devops"
+echo "$PERSON2,i will teach you $PERSON1 friendly"
+echo "$PERSON1,when will you be avilable"
+echo "$PERSON2,i am okay to connect now $PERSON1"
