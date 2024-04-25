20fad6512af2d4975f0696345a90c695de20d90d
ChittiMammu <mammu.malluru@gmail.com>
2024-04-25 20:02:16 +0530

    my first attempt

---
+++ 04-arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+# $1,$2 are the two arguments we are passing
+
+echo "$PERSON1 ,hi will you teach devops"
+echo "$PERSON2,i will teach you $PERSON1 friendly"
+echo "$PERSON1,when will you be avilable"
+echo "$PERSON2,i am okay to connect now $PERSON1"
