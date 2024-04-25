04e4ac89880de4e59e4f697c1a3f85df9de51c93
ChittiMammu <mammu.malluru@gmail.com>
2024-04-25 20:33:17 +0530

    my first attempt

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COLLAGES=("ece" "cse" "eee" "civil")
+
+echo "electronics branch is: ${COLLAGES[0]}"
+echo "computer science branch is: ${COLLAGES[1]}"
+echo "electronics branch is: ${COLLAGES[2]}"
+echo "civil branch is: ${COLLAGES[3]}"
+
+echo "all branches is: ${COLLAGES[@]}"
