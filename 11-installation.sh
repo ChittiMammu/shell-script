20b0c56d4b92b9442003c7b6b0aff4508d437920
ChittiMammu <mammu.malluru@gmail.com>
2024-04-26 22:46:21 +0530

    special variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -9,11 +9,23 @@ if [ $USERID -ne 0 ]
 
 then
 echo "please run the script with root access"
-exit1
+exit 1
 
 else
 echo "you are super user"
 
 fi
 
+
 dnf install mysql -y
+
+if [ $? -ne 0 ] 
+then
+echo "installation of mysql...FAILURE"
+exit 1
+
+else
+echo "installation of mysql ...SUCCESS"
+
+fi
+
