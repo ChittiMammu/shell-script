11c42b8b3cfe9226de4dd7e0bbc5e9f84f076956
ChittiMammu <mammu.malluru@gmail.com>
2024-04-26 22:08:50 +0530

    special variables

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -3,14 +3,13 @@
 NUMBER=$1
 
 if [ $NUMBER -gt 20 ]
-
 then
 
-echo "print $NUMBER is true"
+echo "print $NUMBER is grater than 20"
 
 else
 
-echo "print $NUMBER is false"
+echo "print $NUMBER is less than 20"
 
 fi
 
