505cfc67ec96f9e0d4f773a9952f938cf0364362
ChittiMammu <mammu.malluru@gmail.com>
2024-04-26 21:27:57 +0530

    special variables

---
+++ 08-datatype.sh
@@ -0,0 +1,6 @@
+NO1=$1
+NO2=$2
+
+sub=$((NO1-NO2))
+
+echo "subtraction of $NO1 and $NO2 is: $sub"
---
+++ 09-specialvariables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+#echo "print all variables: $@"
+
+#echo "print number of variables: $#"
+
+#echo "print script name:$0"
+
+#echo "current working directory: $PWD"
+
+echo "which user is running this script: $USER"
+
+#echo "home directory of current user: $HOME"
+
+#echo "hostname: $HOSTNAME"
+
+#echo "process id of the current shell script: $$"
+
+#echo "process id of last background command: $!"
